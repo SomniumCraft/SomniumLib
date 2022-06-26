eb2ee55df637428c5d3499dae3dd16df6b5e8c8f
VitSun666 <vitsun2001@gmail.com>
2022-06-26 15:29:40 +0300

    Ненужное поле

--- src/main/java/ru/somniumcraft/somniumlib/Util/PersistentDataUtils.java
+++ src/main/java/ru/somniumcraft/somniumlib/Util/PersistentDataUtils.java
@@ -9,22 +9,20 @@ import ru.somniumcraft.somniumlib.SomniumLib;
 @UtilityClass
 public class PersistentDataUtils {
 
-    private final SomniumLib plugin = SomniumLib.getInstance();
-
     public <D extends PersistentDataType<T, Z>, T, Z> boolean hasPersistentData (PersistentDataContainer data, String key, D type) {
-        return data.has(new NamespacedKey(plugin, key), type);
+        return data.has(new NamespacedKey(SomniumLib.getInstance(), key), type);
     }
 
     public <D extends PersistentDataType<T, Z>, T, Z> void setPersistentData (PersistentDataContainer data, String key, D type, Z value) {
-        data.set(new NamespacedKey(plugin, key), type, type.getComplexType().cast(value));
+        data.set(new NamespacedKey(SomniumLib.getInstance(), key), type, type.getComplexType().cast(value));
     }
 
     public void removePersistentData (PersistentDataContainer data, String key) {
-        data.remove(new NamespacedKey(plugin, key));
+        data.remove(new NamespacedKey(SomniumLib.getInstance(), key));
     }
 
     public <D extends PersistentDataType<T, Z>, T, Z> Z getPersistentData(PersistentDataContainer data, String key, D type) {
-        return data.get(new NamespacedKey(plugin, key), type);
+        return data.get(new NamespacedKey(SomniumLib.getInstance(), key), type);
     }
 
 }
